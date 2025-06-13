faa191fafb22eaf2533a15d3c82bd77ee39411c7
lian-ju <wjulianpineda@gmail.com>
2025-06-12 23:50:54 -0500

    filename course entity from typeorm fixed

--- microservices/learning/src/infrastructure/typeorm/course/entities/course-entity.ts
+++ microservices/learning/src/infrastructure/typeorm/course/entities/course-entity.ts
@@ -7,14 +7,14 @@ import { Course } from "@domain/course/entities/course";
  * It defines the structure of the Course table in the database.
  */
 export const CourseEntity = new EntitySchema<Course>({
-    name: "Course",
+    name: "courses",
     columns: {
         typeGrade:{
             type: Number,
             nullable: false,
             foreignKey: {
                 name: "FK_Course_TypeGrade",
-                target: "TypeGrade",
+                target: "typeGrades",
             },
         },
         startDate: {
